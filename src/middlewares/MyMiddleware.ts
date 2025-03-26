690b84c27455f87ca6c2a3063a9ce463065d84a0
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-03-26 20:31:18 +0000

    adicionando o arquivo 'middlewares'

---
+++ src/middlewares/MyMiddleware.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from "express";
+
+export function MyMiddleware(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  request.user_id = "123456";
+
+  return next();
+}
