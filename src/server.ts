b2d02f8c67eadd86bd7f232f59336066f60e8071
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-03-28 17:37:05 +0000

    Incluir Zod para validação de dados

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import { routes } from "./routes";
 import { AppError } from "./utils/AppError";
+import { ZodError } from "zod";
 
 const PORT = 3333;
 
@@ -10,10 +11,16 @@ app.use(express.json());
 app.use(routes);
 
 app.use((error: any, request: Request, response: Response, _: NextFunction) => {
-
   if (error instanceof AppError) {
     response.status(error.statusCode).json({ message: error.message });
-    return
+    return;
+  }
+
+  if (error instanceof ZodError) {
+    response
+      .status(400)
+      .json({ message: "Validation Error", issue: error.format() });
+    return;
   }
 
   response.status(500).json({ message: error.message });
